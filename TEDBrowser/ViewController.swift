fe9a3f906644fa912b1b675feb45d14dc376dce0
Nikita Plakhin <nplkhn1@yandex.ru>
2020-08-20 14:35:08 +0300

    Initial Commit

---
+++ TEDBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TEDBrowser
+//
+//  Created by Никита Плахин on 8/20/20.
+//  Copyright © 2020 Никита Плахин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
